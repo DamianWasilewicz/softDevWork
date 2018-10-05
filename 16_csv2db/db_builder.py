e117699357f7504d489b9ccc7a0aad1968a55e6c
Damian Wasilewicz <33820523+DamianWasilewicz@users.noreply.github.com>
2018-10-05 07:47:54 -0400

    Update db_builder.py

--- 16_csv2db/db_builder.py
+++ 16_csv2db/db_builder.py
@@ -1,6 +1,6 @@
 #The Wicked W's - Jeffrey Wu & Damian Wasilewicz
 #SoftDev1 pd0
-#SQLITE3 BASICS
+#K #16: No Trouble  
 #2018-10-04
 
 import sqlite3   #enable control of an sqlite database
@@ -14,22 +14,23 @@ c = db.cursor()               #facilitate db ops
 
 def makeTable(filename):
     with open(filename, 'r') as csvfile: # open csv file
-        courses = csv.DictReader(csvfile) #read it in as a set of dictionaries
+        courses = csv.DictReader(csvfile) #read it in
         #print(reader.get(0))
-        fir = 0 #to only perform this on the first iteration
+        fir = 0 #indicates whether its going through the first time or not, if 0 then first time
         for rec in courses:
             if fir == 0:
-                comm = "CREATE TABLE " + filename[:len(filename) - 4] + " (" #initialize name of table and beginning of creation statement
+                comm = "CREATE TABLE " + filename[:len(filename) - 4] + " (" #beginning of creation statement as a string
                 for col in rec.keys():
-                    comm += "'" + col + "' BLOB, " #add each column in generically typed
+                    comm += "'" + col + "' BLOB, " #add each col in
                     #print(command[:len(command) - 2] + ")")
                 c.execute(comm[:len(comm) - 2] + ")") #execute commmand
-                fir = 1; #make sure this section does not run again
-            comm2 = "INSERT INTO " + filename[:len(filename) - 4] + " VALUES (" #initialize insert statement with table name
+                fir = 1; #since its no longer 0, recognize that its no long firdt time through
+            comm2 = "INSERT INTO " + filename[:len(filename) - 4] + " VALUES (" #initialize insert statement as string
             for info in rec.keys():
                 comm2 += "'" + rec[info] + "', " #add each column of data to statement
-            c.execute(comm2[:len(comm2) - 2] + ")") #execute command
+            c.execute(comm2[:len(comm2) - 2] + ")") #execute command with string 
 f = True
+#to deal with error where it sometimes returns error sahing table already exists and skmetimes that table foesnt exist
 if f:
     c.execute("DROP TABLE peeps;") # Gets rid of peeps table if it already exists
     c.execute("DROP TABLE courses;")
